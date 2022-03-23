e7dc122a6d8c6373413333e46aeeff5e8edc2eef
Taekwan Park <techigh@Taekwans-MacBook-Pro.local>
2022-03-23 20:24:09 +0900

    #1.0 Set Up

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const startAsync = () => {
+    const fontToLoad = [Ionicons.font];
+    const fontPromises = fontToLoad.map(font => Font.loadAsync(font));
+    const logoToload = [require("./assets/logo.png")];
+    const logoPromises = logoToload.map(logo => Asset.loadAsync(logo));
+
+    return Promise.all([...fontPromises, ...logoPromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={startAsync}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./assets/logo.png")} />
+      <Text>Set up</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
